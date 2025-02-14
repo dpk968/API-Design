139ef091de185d1129dea5cc03608c6ec429ca52
dpk968 <deepakkumarg968@gmail.com>
2025-02-14 02:05:52 -0800

    add Producer code

---
+++ banking1/src/main/java/com/banking1/service/BankingService.java
@@ -0,0 +1,64 @@
+package com.banking1.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.banking1.dao.BankingDAOInterface;
+import com.banking1.entity.Employee;
+
+
+@Service
+@Transactional
+public class BankingService implements BankingServiceInterface{
+	
+	@Autowired
+	private BankingDAOInterface bDao;
+
+	@Override
+	public String createProfileService(Employee emp) {
+		bDao.save(emp);
+		return "user registered successfully";
+	}
+
+	@Override
+	public List<Employee> getAllRecordService() {
+		// TODO Auto-generated method stub
+		return bDao.findAll();
+	}
+
+	@Override
+	public String editRecordService(Employee emp) {
+		bDao.saveAndFlush(emp);
+		return "record edited";
+	}
+
+	@Override
+	public String deleteRecordService(String email) {
+		bDao.deleteById(email);
+		return "record deleted";
+	}
+
+	@Override
+	public Employee getEmployeeRecordByIdService(String email) {
+	   Optional<Employee> ee=	  bDao.findById(email);
+		return ee.get();
+	}
+	
+
+	@Override
+	public Employee findByEmailAndPassword(String email, String password) {
+		return bDao.findByEmailAndPassword(email,password);
+	}
+
+}
+
+
+
+
+
+
+
